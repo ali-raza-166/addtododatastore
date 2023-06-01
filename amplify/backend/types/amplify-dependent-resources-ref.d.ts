283bb92b49be91760d33d6681852edb6aa894931
aliraza.amanat929@gmail.com <razaa13@NETSOLPK.COM>
2023-06-01 16:55:59 +0500

    First

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "tododatastoreali": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
